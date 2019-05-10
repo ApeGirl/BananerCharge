1e3164e834fad410a4dea64da1026bd5522222ff
unknown <xiaoyue.wang@HT340-WIN7.JSTINNO>
2019-05-10 15:11:31 +0800

    pay

--- app/src/main/java/Util/URLUtils.java
+++ app/src/main/java/Util/URLUtils.java
@@ -19,6 +19,7 @@ public class URLUtils {
     public static final String ORDER_WECHAT_SUCCESS = "http://39.97.180.130:8080/renren-fast/app/order/wechatSuccess";
 
     public static final String ACTION_REQUEST_SUCCESS_RECEIVER = "action_request_success";
+    public static final String ACTION_CREATE_ORDER_RECEIVER = "action_create_order";
     public enum RequestType{
         GOODS_LIST, LOGIN, GOODS_INFO, SEND_CODE, ALI_PAY, WECHAT_PAY, GET_ALI_PAY_INFO, GET_WECHAT_PAY_INFO, CREAT_ORDER, ORDER_INFO, ORDER_LIST
     }
--- app/src/main/java/Util/Utils.java
+++ app/src/main/java/Util/Utils.java
@@ -1,9 +1,14 @@
 package Util;
 
+import android.content.Context;
 import android.content.Intent;
 import android.util.Base64;
 import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.ape.bananarecharge.Controller.GoodsManager;
 import com.ape.bananarecharge.Datamodel.GoodsInfo;
 
 import java.io.ByteArrayInputStream;
@@ -11,6 +16,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.Map;
 
 /**
  * Created by xiaoyue.wang on 2019/5/7.
@@ -21,13 +27,16 @@ public class Utils {
     public static final String BUY_COUNT = "buy_count";
     public static final String UER_ACCOUNT = "usr_account";
     public static final String BUY_TYPE = "usr_account";
+    public static final String ORDER_ID = "orderid";
 
     public static final int NO_TYPE = -1;
     public static final int DIRECT_BUY = 1;
     public static final int SHARE_BUY = 2;
     public static final int WACHAT_PAY = 3;
     public static final int Ali_PAY = 4;
+    public static int pay_type = -1;
 
+    private static RelativeLayout state;
     private static final String TAG = "Utils2";
     public static String Object2String(Object object, String activity) {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
@@ -65,6 +74,25 @@ public class Utils {
             return null;
         }
 
+    }
+
+    public static void createOrderPay(Context context, int type, Map map){
+        GoodsManager goodsManager = new GoodsManager(context);
+        if (type == WACHAT_PAY) {
+            goodsManager.doPostRequest(map, URLUtils.ORDER_WECHAT_PAY, URLUtils.RequestType.WECHAT_PAY);
+        } else if (type == Ali_PAY) {
+            goodsManager.doPostRequest(map, URLUtils.ORDER_ALIPAY, URLUtils.RequestType.ALI_PAY);
+        } else {
+            Toast.makeText(context, "请选择支付方式", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static void setPayWaitLayout(RelativeLayout progressBar) {
+        state = progressBar;
+    }
+
+    public static RelativeLayout getPayWaitState() {
+        return state;
     }
 
 }
